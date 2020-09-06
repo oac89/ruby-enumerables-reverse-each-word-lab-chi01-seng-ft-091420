7e19c7a9561f9685d94ae2b5d4e26c904063ff5d
Oscar A Castro <oacastro2@gmail.com>
2020-09-06 19:47:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,9 +7,8 @@ def reverse_each_word(sentance1)
 end 
 
 def reverse_each_word(sentance2)
-     array_reverse = [sentance2]
+     
     array_reverse.collect do 
       |string| sentance2 reverse!(string)
     end
   end     
-reverse_each_word(sentance2)
