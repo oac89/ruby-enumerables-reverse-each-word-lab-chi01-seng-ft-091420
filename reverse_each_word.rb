acc675cb200379febea05395468f542d89d956d4
Oscar A Castro <oacastro2@gmail.com>
2020-09-06 20:40:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(sentance2)
     ar2 = sentence2.split(  )
       ar2.collect do 
-      |string| string.reverse.join  
+      |string| string.reverse.join.string 
       
       
     end
