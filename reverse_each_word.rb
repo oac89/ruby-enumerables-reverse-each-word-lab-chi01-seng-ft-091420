cd83730e8c28dc440aac577f12a52a0bfc874d64
Oscar A Castro <oacastro2@gmail.com>
2020-09-06 20:14:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
 def reverse_each_word(sentance1)
    
-      ar1 = %w{sentance1} 
+      ar1 = %w{sentence1} 
       ar1.each do |string|
         string.reverse! << new_arr
       end 
 end 
 
 def reverse_each_word(sentance2)
-    ar2 = %w{sentance2}
+    ar2 = %w{sentence2}
     ar2.collect do 
       |string| string.reverse!
     end
